137b9d33729ff33ca66e701b9912ab5e5af68638
KrasimirTerziev <kdterziev@abv.bg>
2016-08-28 15:05:48 +0300

    Coment_ Model

--- models/CommentModel.php
+++ models/CommentModel.php
@@ -8,13 +8,15 @@ class CommentModel extends BaseModel {
      * @return int | Returns the comment id on succsess
      */
     public function save($comment, $post_id, $user_id) {
+        $usersModel = new UsersModel;
+        $userData = $usersModel->getById($user_id);
+        $author = $userData["full_name"];
 
         $post_id = (int) $post_id;
 
         $comment = strip_tags($comment, '<a><p>');
         $comment = self::$db->escape_string($comment);
-        $author = $user_id;
-        $sql = "INSERT INTO comments (comment, post_id) VALUES ('{$comment}', '{$post_id}')";
+        $sql = "INSERT INTO comments (comment, post_id, author) VALUES ('{$comment}', '{$post_id}', '{$author}')";
         self::$db->query($sql);
 
         return self::$db->insert_id;
