375793cd8125efa096447072c3db5cd77fd84928
RosenRaikov <rosen_raikov@hotmail.com>
2016-08-31 13:28:17 +0300

    Update create.php

--- views/posts/create.php
+++ views/posts/create.php
@@ -3,10 +3,10 @@
 <div class="new-post">
     <form class="post-form" method="post">
         <li class="post-elements">
-            <div>Title:</div>
-            <input type="text" name="post_title" />
-            <div >Content:</div>
-            <textarea rows="10" name="post_content"></textarea>
+            <div class="post-text">Title:</div>
+            <input class="post_title" type="text" name="post_title" />
+            <div class="post-text">Content:</div>
+            <textarea class="post-content" name="post_content"></textarea>
         </li>
         <div class="button-second"><input class="submit" type="submit" value="Create" />
         <a href="<?=APP_ROOT?>/posts">Cancel</a></div>
